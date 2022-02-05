38e693afb74048222cea4c977ee62e610f0ff43f
lyleokoth <okothlyleochieng@gmail.com>
2022-02-05 08:52:18 +0300

    Finished preliminary design

--- app/routes.py
+++ app/routes.py
@@ -8,5 +8,17 @@ from flask import url_for
 @app.route('/home', methods=['GET'])
 @app.route('/dashboard', methods=['GET'])
 def dashboard_page():
-    return 'Hello from flask'
+    return render_template('dashboard_page.html')
+
+@app.route('/startups', methods=['GET'])
+def startups_page():
+    return render_template('startups_page.html')
+
+@app.route('/investors', methods=['GET'])
+def investor_page():
+    return render_template('investors_page.html')
+
+@app.route('/business_hubs', methods=['GET'])
+def business_hubs_page():
+    return render_template('business_hubs_page.html')
 
